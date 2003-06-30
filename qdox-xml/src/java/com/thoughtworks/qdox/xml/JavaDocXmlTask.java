863137a4d644db3c7c7329c692bf0776455b498f
mdub <mdub@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2003-06-30 15:37:15 +0000

    Initial revision

---
+++ qdox-xml/src/java/com/thoughtworks/qdox/xml/JavaDocXmlTask.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.qdox.xml;
+
+import com.thoughtworks.qdox.ant.AbstractQdoxTask;
+import com.thoughtworks.qdox.model.JavaSource;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.IOException;
+
+import org.apache.tools.ant.BuildException;
+
+public class JavaDocXmlTask extends AbstractQdoxTask {
+
+	private File dest;
+
+	public void setDest(File dest) {
+		this.dest = dest;
+	}
+
+	protected void validateAttributes() {
+		super.validateAttributes();
+		if (dest == null) {
+			throw new BuildException("no \"dest\" specified");
+		}
+	}
+
+	protected void processSources(JavaSource[] sources) {
+		try {
+			Writer destWriter = new FileWriter(dest);
+			JavaDocXmlGenerator xmlGenerator =
+				new JavaDocXmlGenerator(new TextXmlHandler(destWriter, "  "));
+			xmlGenerator.write(sources);
+		} catch (IOException e) {
+			throw new BuildException(e);
+		}
+	}
+
+}
---
+++ qdox-xml/src/java/com/thoughtworks/qdox/xml/XmlHandlerException.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.qdox.xml;
+
+public class XmlHandlerException extends RuntimeException {
+
+	public XmlHandlerException(String message) {
+		super(message);
+	}
+
+	public XmlHandlerException(Throwable cause) {
+		super(cause);
+	}
+
+	public XmlHandlerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
