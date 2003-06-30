352d4c1be3b7d21ea00e7fe767c6daf0a0e60a3b
nobody <nobody@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2003-06-30 15:37:16 +0000

    This commit was manufactured by cvs2svn to create tag 'initial'.

--- qdox/src/java/com/thoughtworks/qdox/Searcher.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.qdox;
-
-import com.thoughtworks.qdox.model.JavaClass;
-
-public interface Searcher {
-
-	boolean eval(JavaClass cls);
-
-}
--- qdox/src/java/com/thoughtworks/qdox/directorywalker/FileVisitor.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.qdox.directorywalker;
-
-import java.io.File;
-
-public interface FileVisitor {
-
-	void visitFile(File file);
-
-}
