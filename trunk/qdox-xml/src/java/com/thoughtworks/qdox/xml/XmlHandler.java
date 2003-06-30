4774b90d5350e0b0817f3fd6039406a5b2a0674a
mdub <mdub@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2003-06-30 15:37:15 +0000

    Initial revision

---
+++ trunk/qdox-xml/src/java/com/thoughtworks/qdox/xml/XmlHandler.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.qdox.xml;
+
+// This is a simplified version of the SAX ContentHandler interface, sufficient for
+// the purposes of dealing with QDox serialization.
+
+/**
+ * Interface provided by objects that can serialize JavaDoc information
+ * as XML (or in an XML-like form).
+ */
+public interface XmlHandler {
+
+	void startDocument();
+
+	void startElement(String name);
+
+	void addContent(String text);
+
+	void endElement();
+
+	void endDocument();
+
+}
