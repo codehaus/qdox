5ccb2fd86d087eb5eaa21779519dac96cfbd827c
nobody <nobody@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2003-07-09 11:56:21 +0000

    This commit was manufactured by cvs2svn to create branch 'XJAVADOC_MARRIAGE'.

---
+++ branches/XJAVADOC_MARRIAGE/qdox/src/java/com/thoughtworks/qdox/model/JavaClassParent.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.qdox.model;
+
+public interface JavaClassParent {
+
+	public String asClassNamespace();
+
+	public JavaSource getParentSource();
+
+	public void addClass(JavaClass cls);
+
+	public JavaClass[] getClasses();
+
+}
