bb2c0b4b9cf01d6f62a4b5149d17ad537654dd7a
paul <paul@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2009-09-03 23:15:49 +0000

    [maven-scm] copy for tag qdox-1.10-attempt3

---
+++ tags/qdox-1.10-attempt3/src/java/com/thoughtworks/qdox/model/Annotation.java
@@ -0,0 +1,128 @@
+package com.thoughtworks.qdox.model;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.thoughtworks.qdox.model.annotation.AnnotationValue;
+import com.thoughtworks.qdox.model.annotation.AnnotationVisitor;
+
+/**
+ * 
+ * @author Eric Redmond
+ */
+public class Annotation implements AnnotationValue, Serializable
+{
+    private final Type type;
+    private final int lineNumber;
+
+    /**
+     * Annotation properties as AnnotationValues
+     * <p>
+     * This map contains the parsed AnnotationValue for each property and allows
+     * access to the full parse tree, including typerefs and expressions.
+     */
+    private final Map properties = new LinkedHashMap();
+
+    /**
+     * Annotation properties as Parameters
+     */
+    private final Map namedParameters = new LinkedHashMap();
+
+    private AbstractBaseJavaEntity context;
+
+    public Annotation(Type type,
+            AbstractBaseJavaEntity context,
+            Map namedParameters,
+            int lineNumber)
+	{
+        this.type = type;
+        this.context = context;
+        this.lineNumber = lineNumber;
+        
+        if(properties != null) {
+            for(Iterator i = this.properties.entrySet().iterator(); i.hasNext(); ) {
+                Entry entry = (Entry) i.next();
+                String name = (String) entry.getKey();
+                AnnotationValue value = (AnnotationValue) entry.getValue();
+                
+                setProperty(name, value);
+            }
+        }
+	}
+
+    public Annotation( Type type, int line ) {
+        this(type, null, null, line);
+    }
+
+    public void setProperty(String name, AnnotationValue value) {
+        properties.put( name, value );
+        namedParameters.put( name, value.getParameterValue() );
+    }
+
+    /**
+     * @return the annotation type
+     */
+    public Type getType() {
+    	return type;
+    }
+
+    /**
+     * @param key name of a named-parameter
+     * @return the corresponding value,
+     *   or null if no such named-parameter was present
+     */
+    public Object getNamedParameter(String key) {
+    	return namedParameters.get( key );
+    }
+
+    /**
+     * @return a Map containing all the named-parameters
+     */
+    public Map getNamedParameterMap() {
+    	return namedParameters;
+    }
+
+    public final AbstractBaseJavaEntity getContext() {
+        return context;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public Object accept( AnnotationVisitor visitor ) {
+        return visitor.visitAnnotation( this );
+    }
+
+    public Object getParameterValue() {
+        return this;
+    }
+    
+    public Map getPropertyMap() {
+        return properties;
+    }
+    
+    public AnnotationValue getProperty(String name) {
+        return (AnnotationValue) properties.get( name );
+    }
+
+    public void setContext( AbstractBaseJavaEntity context ) {
+        this.context = context;
+    }
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append('@');
+        result.append(type.getValue());
+        result.append('(');
+        if( !namedParameters.isEmpty() ) {
+            for(Iterator i = namedParameters.entrySet().iterator(); i.hasNext();) result.append( i.next() + ",");
+            result.deleteCharAt( result.length()-1 );
+        }
+        result.append(')');
+        return result.toString();
+    }
+}
---
+++ tags/qdox-1.10-attempt3/src/test/com/thoughtworks/qdox/model/JavaSourceTest.java
@@ -0,0 +1,219 @@
+package com.thoughtworks.qdox.model;
+
+import junit.framework.TestCase;
+
+import java.util.HashMap;
+
+public class JavaSourceTest extends TestCase {
+
+    private JavaSource source;
+
+    public JavaSourceTest(String s) {
+        super(s);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        source = new JavaSource();
+        source.setClassLibrary(new ClassLibrary());
+    }
+
+    public void testToStringOneClass() throws Exception {
+        JavaClass cls = new JavaClass();
+        cls.setName("MyClass");
+        source.addClass(cls);
+        String expected = ""
+                + "class MyClass {\n"
+                + "\n"
+                + "}\n";
+        assertEquals(expected, source.toString());
+    }
+
+    public void testToStringMultipleClass() throws Exception {
+        JavaClass cls1 = new JavaClass();
+        cls1.setName("MyClass1");
+        source.addClass(cls1);
+        JavaClass cls2 = new JavaClass();
+        cls2.setName("MyClass2");
+        source.addClass(cls2);
+        JavaClass cls3 = new JavaClass();
+        cls3.setName("MyClass3");
+        source.addClass(cls3);
+
+        String expected = ""
+                + "class MyClass1 {\n"
+                + "\n"
+                + "}\n"
+                + "\n"
+                + "class MyClass2 {\n"
+                + "\n"
+                + "}\n"
+                + "\n"
+                + "class MyClass3 {\n"
+                + "\n"
+                + "}\n";
+        assertEquals(expected, source.toString());
+    }
+
+    public void testToStringPackage() throws Exception {
+        JavaClass cls = new JavaClass();
+        cls.setName("MyClass");
+        source.addClass(cls);
+        source.setPackage(new JavaPackage("com.thing", new HashMap()));
+        String expected = ""
+                + "package com.thing;\n"
+                + "\n"
+                + "class MyClass {\n"
+                + "\n"
+                + "}\n";
+        assertEquals(expected, source.toString());
+    }
+
+    public void testToStringImport() throws Exception {
+        JavaClass cls = new JavaClass();
+        cls.setName("MyClass");
+        source.addClass(cls);
+        source.addImport("java.util.*");
+        String expected = ""
+                + "import java.util.*;\n"
+                + "\n"
+                + "class MyClass {\n"
+                + "\n"
+                + "}\n";
+        assertEquals(expected, source.toString());
+    }
+
+    public void testToStringMultipleImports() throws Exception {
+        JavaClass cls = new JavaClass();
+        cls.setName("MyClass");
+        source.addClass(cls);
+        source.addImport("java.util.*");
+        source.addImport("com.blah.Thing");
+        source.addImport("xxx");
+        String expected = ""
+                + "import java.util.*;\n"
+                + "import com.blah.Thing;\n"
+                + "import xxx;\n"
+                + "\n"
+                + "class MyClass {\n"
+                + "\n"
+                + "}\n";
+        assertEquals(expected, source.toString());
+    }
+
+    public void testToStringImportAndPackage() throws Exception {
+        JavaClass cls = new JavaClass();
+        cls.setName("MyClass");
+        source.addClass(cls);
+        source.addImport("java.util.*");
+        source.setPackage(new JavaPackage("com.moo", new HashMap()));
+        String expected = ""
+                + "package com.moo;\n"
+                + "\n"
+                + "import java.util.*;\n"
+                + "\n"
+                + "class MyClass {\n"
+                + "\n"
+                + "}\n";
+        assertEquals(expected, source.toString());
+    }
+
+    public void testGetClassNamePrefix() {
+        assertEquals("", source.getClassNamePrefix());
+        source.setPackage(new JavaPackage("foo.bar", new HashMap()));
+        assertEquals("foo.bar.", source.getClassNamePrefix());
+       }
+    
+    public void testResolveJavaPrimitive() throws Exception {
+        source.addImport("bogus.int");
+        source.addImport("bogus.double");
+        String[] primitives = new String[]{
+            "boolean", "byte", "char", "double",
+            "float", "int", "long", "short", "void"
+        };
+        for (int i = 0; i < primitives.length; i++) {
+            assertEquals(primitives[i], source.resolveType(primitives[i]));
+        }
+    }
+
+    public void testDontResolveMissingClasses() throws Exception {
+        assertEquals(null, source.resolveType("not.Found"));
+    }
+
+    public void testResolveFullyQualifiedName() throws Exception {
+        source.getClassLibrary().add("open.Bar");
+        assertEquals("open.Bar", source.resolveType("open.Bar"));
+    }
+    
+    public void testResolveFullyQualifiedImport() throws Exception {
+        source.addImport("foo.Bar");
+        source.getClassLibrary().add("foo.Bar");
+        assertEquals("foo.Bar", source.resolveType("Bar"));
+    }
+
+    public void testResolveChooseFirstMatchingImport() throws Exception {
+        source.addImport("bogus.package.MyType");
+        source.addImport("com.thoughtworks.qdox.model.Type");
+        source.addImport("another.package.Type");
+        source.getClassLibrary().add("bogus.package.MyType");
+        source.getClassLibrary().add("com.thoughtworks.qdox.model.Type");
+        source.getClassLibrary().add("another.package.Type");
+        assertEquals("com.thoughtworks.qdox.model.Type", source.resolveType("Type"));
+    }
+
+    public void testResolveSamePackage() throws Exception {
+        source.setPackage(new JavaPackage("foo", new HashMap()));
+        source.getClassLibrary().add("foo.Bar");
+        assertEquals("foo.Bar", source.resolveType("Bar"));
+    }
+
+    public void testResolveFullyQualifiedTrumpsSamePackage() throws Exception {
+        source.setPackage(new JavaPackage("foo", new HashMap()));
+        source.getClassLibrary().add("foo.Bar");
+        source.getClassLibrary().add("open.Bar");
+        assertEquals("open.Bar", source.resolveType("open.Bar"));
+    }
+
+    public void testResolveFullyQualifiedTrumpsWildCard() throws Exception {
+        source.addImport("bar.Bar");
+        source.addImport("foo.Bar");
+        source.getClassLibrary().add("foo.*");
+        source.getClassLibrary().add("bar.Bar");
+        assertEquals("bar.Bar", source.resolveType("Bar"));
+    }
+
+    public void testResolveWildcard() throws Exception {
+        source.getClassLibrary().add("foo.Bar");
+        source.addImport("foo.*");
+        assertEquals("foo.Bar", source.resolveType("Bar"));
+    }
+
+    public void testResolveJavaLangClass() throws Exception {
+        source.getClassLibrary().add("java.lang.System");
+        assertEquals("java.lang.System", source.resolveType("System"));
+    }
+
+    public void testResolveSamePackageTrumpsWildcard() throws Exception {
+        source.addImport("com.thoughtworks.qdox.model.Type");
+        source.addImport("foo.*");
+        source.getClassLibrary().add("com.thoughtworks.qdox.model.Type");
+        source.getClassLibrary().add("foo.Type");
+        assertEquals("com.thoughtworks.qdox.model.Type", source.resolveType("Type"));
+    }
+
+    public void testResolveFullyQualifiedInnerClass() throws Exception {
+        source.setPackage(new JavaPackage("foo", new HashMap()));
+        source.getClassLibrary().add("foo.Bar$Fnord");
+        assertEquals("foo.Bar$Fnord", source.resolveType("foo.Bar.Fnord"));
+    }
+
+    public void testResolvePartiallySpecifiedInnerClass() throws Exception {
+        source.setPackage(new JavaPackage("foo", new HashMap()));
+        source.addImport("java.util.*");
+        source.getClassLibrary().add("foo.Bar$Fnord");
+        source.getClassLibrary().addDefaultLoader();
+        assertEquals("foo.Bar$Fnord", source.resolveType("Bar.Fnord"));
+        assertEquals("java.util.Map$Entry", source.resolveType("Map.Entry"));
+    }
+    
+}
